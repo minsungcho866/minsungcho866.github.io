2f55a3572076d4aeeb551aad70d3981007f2faf6
minsungcho866 <chom866@newschool.edu>
2023-11-21 16:58:25 -0500

    entry10

---
+++ entries/entry10/main.js
@@ -0,0 +1,5 @@
+function playAudio(url) {
+    new Audio(url).play();
+  }
+
+  alert("Hello! Please press 'OK' if you want to know which song I chose for this week. This is a song for the disny movie MOANA and is my favorie disney song. It shows the courage of the character and her wish to move on. I thought that this song matches well with my situation. ");
